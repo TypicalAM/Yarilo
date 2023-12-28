147fb74a49e486facd087ff14c5ceafd1cd824eb
TypicalAM <tpam_piaseczny@protonmail.com>
2023-12-28 16:59:12 +0100

    Test libtins setup (not working)

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,80 @@
-#include "ManagementLayer.h"
-#include "RadiotapLayer.h"
-#include <Layer.h>
-#include <Packet.h>
-#include <PcapFileDevice.h>
-#include <ProtocolType.h>
-#include <RawPacket.h>
 #include <cstdint>
+#include <iomanip>
 #include <iostream>
-#include <ostream>
-#include <string>
+#include <tins/dhcp.h>
+#include <tins/ethernetII.h>
+#include <tins/hw_address.h>
+#include <tins/macros.h>
+#include <tins/pdu.h>
+#include <tins/sniffer.h>
+#include <tins/tins.h>
 
-void ProcessRawPacket(pcpp::RawPacket *rawPacket) {
-  uint8_t *raw = (uint8_t *)rawPacket->getRawData();
-  size_t len = rawPacket->getRawDataLen();
-  pcpp::Layer *radio = new RadiotapLayer(raw, len, nullptr);
-  pcpp::Layer *mgmt = new ManagementLayer(
-      raw + radio->getHeaderLen(), len - radio->getHeaderLen(), radio, nullptr);
-
-  pcpp::Packet pkt;
-  pkt.addLayer(radio);
-  pkt.addLayer(mgmt);
-  std::cout << pkt.toString() << std::endl;
+void printHex(uint8_t *data, int i) {
+  for (int j = 1; j < i + 1; ++j) {
+    std::cout << std::hex << std::setw(2) << std::setfill('0')
+              << static_cast<int>(data[j - 1]) << std::dec;
+    std::cout << " ";
+    if (j % 8 == 0 && j != 0) {
+      std::cout << "  ";
+    }
+    if (j % 16 == 0 && j != 0) {
+      std::cout << std::endl;
+    }
+  }
+  std::cout << std::endl;
 }
 
+Tins::EthernetII make_eth_packet(Tins::Dot11Data &dot11) {
+  if (dot11.from_ds() && !dot11.to_ds()) {
+    return Tins::EthernetII(dot11.addr1(), dot11.addr3());
+  } else if (!dot11.from_ds() && dot11.to_ds()) {
+    return Tins::EthernetII(dot11.addr3(), dot11.addr2());
+  } else {
+    return Tins::EthernetII(dot11.addr1(), dot11.addr2());
+  }
+}
+
+class TestingDecrypter {
+public:
+  TestingDecrypter() {
+    const Tins::HWAddress<6> bssid("00:0c:41:82:b2:55");
+    const std::string passwd("Induction");
+    Tins::Crypto::WEPDecrypter decrypter;
+    dec.add_password(bssid, passwd);
+
+    Tins::FileSniffer sniffer("pcap/wpa_induction.pcap");
+    sniffer.sniff_loop(
+        Tins::make_sniffer_handler(this, &TestingDecrypter::callback));
+  }
+
+  bool callback(Tins::PDU &pkt) {
+    count++;
+    std::vector<uint8_t> buffer = pkt.serialize();
+    std::cout << "This is packet: " << count << " with encrypted size "
+              << buffer.size() << std::endl;
+    printHex(buffer.data(), buffer.size());
+    dec.decrypt(pkt);
+    Tins::Dot11Data *data = pkt.find_pdu<Tins::Dot11Data>();
+    if (!data)
+      return true;
+
+    std::vector<uint8_t> bufferDec = pkt.serialize();
+    if (count != 209)
+      return true;
+
+    std::cout << "This is packet: " << count << " with unencrypted size "
+              << bufferDec.size() << std::endl;
+    printHex(bufferDec.data(), bufferDec.size());
+    return false;
+  };
+
+private:
+  Tins::Crypto::WEPDecrypter dec;
+  int count = 0;
+};
+
 int main(int argc, char *argv[]) {
-  pcpp::PcapFileReaderDevice reader("pcap/handshake.pcap");
-  if (!reader.open()) {
-    std::cerr << "Error opening the pcap file" << std::endl;
-    return 1;
-  }
-
-  pcpp::RawPacket rawPacket;
-  int i = 0;
-  while (reader.getNextPacket(rawPacket)) {
-    if (i == 5)
-      break;
-    ProcessRawPacket(&rawPacket);
-    i++;
-  }
-
-  reader.close();
+  std::cout << "Starting..." << std::endl;
+  TestingDecrypter lol;
   return 0;
 }
