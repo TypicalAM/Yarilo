1aa68af18c8ca3ac913d6dce0900628ccf2741c9
zecozejak <alexcros2002@gmail.com>
2024-01-16 11:32:26 +0100

    test front

--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -1,39 +1,42 @@
-import { ClientInfo, DeauthRequest, DecryptRequest, DecryptState, Empty, File, NetworkName, } from './packets_pb';
+import { ClientInfo, DeauthRequest, DecryptRequest, DecryptState, Empty, File, NetworkName } from './packets_pb';
 import { SniffinsonClient } from './packets_grpc_web_pb';
 
 var client = new SniffinsonClient('http://localhost:8080');
 
+const logBoxContainer = document.getElementById('logBoxContainer');
+const logBox = document.getElementById('logBox');
+const messageWindow = document.getElementById('messageWindow');
+
 function getSelectedNetwork(): string {
-    let networkTable = document.getElementById("networks_list").firstChild;
-    let selectedNetwork = "";
+    let networkTable = document.getElementById('networks_list').firstChild;
+    let selectedNetwork = '';
     networkTable.childNodes.forEach((row) => {
         let radio = row.lastChild.lastChild as HTMLInputElement; // <tr> <td> text <input>
         if (radio.checked) selectedNetwork = row.textContent;
-    })
+    });
     return selectedNetwork;
 }
 
 function getSelectedRecording(): string {
-    let fileTable = document.getElementById("files_list").firstChild;
-    let selectedFile = "";
+    let fileTable = document.getElementById('files_list').firstChild;
+    let selectedFile = '';
     fileTable.childNodes.forEach((row) => {
         let radio = row.lastChild.lastChild as HTMLInputElement; // <tr> <td> text <input>
         if (radio.checked) selectedFile = row.textContent;
-    })
+    });
     return selectedFile;
 }
 
-
 function getNetworks() {
-    console.log("Getting all the deteected networks");
-    client.getAllAccessPoints(new Empty(), {}, function(err, response) {
+    console.log('Getting all the detected networks');
+    client.getAllAccessPoints(new Empty(), {}, function (err, response) {
         if (err) {
-            console.error("Got err: ", err)
+            console.error('Got err: ', err);
             return;
         }
 
         let ssidList = response.getNamesList();
-        let networkTable = document.getElementById("networks_list");
+        let networkTable = document.getElementById('networks_list');
         networkTable.innerHTML = ''; // Remove everything that was there prev
         let table = document.createElement('table');
         for (const ssid of ssidList) {
@@ -41,46 +44,46 @@ function getNetworks() {
             let data = document.createElement('td');
             data.textContent = ssid;
 
-            let input = document.createElement("input");
-            input.type = "radio";
-            data.append(input)
-            row.appendChild(data)
-            table.appendChild(row)
+            let input = document.createElement('input');
+            input.type = 'radio';
+            data.append(input);
+            row.appendChild(data);
+            table.appendChild(row);
         }
 
         networkTable.appendChild(table);
-        console.log("Got access point list: ", ssidList);
+        console.log('Got access point list: ', ssidList);
     });
-};
+}
 
 function getNetworkByName() {
-    console.log("Getting a specific network");
+    console.log('Getting a specific network');
     let selectedNetwork = getSelectedNetwork();
     let request = new NetworkName();
     request.setSsid(selectedNetwork);
-    console.log("Sending request for the network: ", selectedNetwork);
+    console.log('Sending request for the network: ', selectedNetwork);
 
     client.getAccessPoint(request, {}, (err, response) => {
         if (err) {
-            console.error("Got err: ", err)
+            console.error('Got err: ', err);
             return;
         }
 
-        let netInfo = document.getElementById("net_info");
-        netInfo.innerHTML = "";
+        let netInfo = document.getElementById('net_info');
+        netInfo.innerHTML = '';
         let table = document.createElement('table');
 
         let clientInfos = response.getClientsList().map((info: ClientInfo) => {
-            return `Client ${info.getAddr()} is decrypted: ${info.getIsDecrypted()} and has got ${info.getHandshakeNum()} handshakes`
-        })
+            return `Client ${info.getAddr()} is decrypted: ${info.getIsDecrypted()} and has got ${info.getHandshakeNum()} handshakes`;
+        });
 
-        let apinfo = `Name: ${response.getName()}, BSSID: ${response.getBssid()}, Channel: ${response.getChannel()}, Got ${response.getEncryptedPacketCount()} encrypted and ${response.getDecryptedPacketCount()} decrypted packets`
+        let apinfo = `Name: ${response.getName()}, BSSID: ${response.getBssid()}, Channel: ${response.getChannel()}, Got ${response.getEncryptedPacketCount()} encrypted and ${response.getDecryptedPacketCount()} decrypted packets`;
         for (const elem of [apinfo, ...clientInfos]) {
             let row = document.createElement('tr');
             let data = document.createElement('td');
             data.textContent = elem.toString();
-            row.appendChild(data)
-            table.appendChild(row)
+            row.appendChild(data);
+            table.appendChild(row);
         }
 
         netInfo.appendChild(table);
@@ -88,126 +91,131 @@ function getNetworkByName() {
 }
 
 function tryInputPassword() {
-    console.log("Trying to input a password");
+    console.log('Trying to input a password');
     let selectedNetwork = getSelectedNetwork();
-    let input = document.getElementById("password_text") as HTMLInputElement;
+    let input = document.getElementById('password_text') as HTMLInputElement;
     let request = new DecryptRequest();
     request.setSsid(selectedNetwork);
     request.setPasswd(input.value.trim());
 
-    console.log("Trying to provide a password for the network", selectedNetwork);
-    console.log("With password", input.value.trim());
+    console.log('Trying to provide a password for the network', selectedNetwork);
+    console.log('With password', input.value.trim());
 
     client.providePassword(request, {}, (err, response) => {
         if (err) {
-            console.error("Got err: ", err)
+            console.error('Got err: ', err);
             return;
         }
 
         switch (response.getState()) {
             case DecryptState.SUCCESS:
-                console.log("Success")
+                console.log('Success');
                 break;
             case DecryptState.WRONG_OR_NO_DATA:
-                console.log("The key is wrong or we didn't have 4 handshakes in any client")
+                console.log("The key is wrong or we didn't have 4 handshakes in any client");
                 break;
             case DecryptState.ALREADY_DECRYPTED:
-                console.log("Already decrypted, idiot")
-                break
+                console.log('Already decrypted, idiot');
+                break;
             case DecryptState.WRONG_NETWORK_NAME:
-                console.log("Wrong network name, how did you even manage to do that?")
-                break
+                console.log('Wrong network name, how did you even manage to do that?');
+                break;
         }
-    })
+    });
 }
 
 function tryGetStream() {
-    console.log("Trying to get the decrypted stream");
+    console.log('Trying to get the decrypted stream');
     let selectedNetwork = getSelectedNetwork();
     let request = new NetworkName();
     request.setSsid(selectedNetwork);
 
     let stream = client.getDecryptedPackets(request, {});
+
+    logBox.innerHTML = '';
+
     stream.on('data', (response) => {
         let from = response.getFrom();
         let to = response.getTo();
-        console.log(response.getProtocol(), "from", from.getMacaddress(), from.getIpv4address(), from.getPort(), "to", to.getMacaddress(), to.getIpv4address(), to.getPort());
-    })
+        let logMessage = `${response.getProtocol()} from ${from.getMacaddress()}, ${from.getIpv4address()}, ${from.getPort()} to ${to.getMacaddress()}, ${to.getIpv4address()}, ${to.getPort()}`;
 
-    stream.on('end', () => {
-        console.log("end");
+        logBox.innerHTML += `<p>${logMessage}</p>`;
+        console.log(logMessage);
     });
 
-    let cancelBtn = document.getElementById('cancel_stream')
-    cancelBtn.className = "btn btn-warning";
+    stream.on('end', () => {
+        console.log('end');
+    });
+
+    let cancelBtn = document.getElementById('cancel_stream');
+    cancelBtn.className = 'btn btn-warning';
     cancelBtn.addEventListener('click', () => {
-        console.log("Cancelling stream")
-        stream.cancel()
-        console.log("Stream cancelled")
-        cancelBtn.className = "btn btn-primary";
-    })
+        console.log('Cancelling stream');
+        stream.cancel();
+        console.log('Stream cancelled');
+        cancelBtn.className = 'btn btn-primary';
+    });
+
+    logBoxContainer.scrollTop = logBoxContainer.scrollHeight;
 }
 
 function ignoreNetwork() {
-    console.log("Trying to send a network to ignore")
-    let input = document.getElementById("ignore_network_text") as HTMLInputElement;
+    console.log('Trying to send a network to ignore');
+    let input = document.getElementById('ignore_network_text') as HTMLInputElement;
     let ssid = input.value;
 
-    let request = new NetworkName()
+    let request = new NetworkName();
     request.setSsid(ssid);
-    client.ignoreNetwork(request, {}, function(err, response) {
+    client.ignoreNetwork(request, {}, function (err, response) {
         if (err) {
-            console.error("Got err: ", err)
+            console.error('Got err: ', err);
             return;
         }
 
-        // This doesn't return anythin
-    })
+        // This doesn't return anything
+    });
 }
 
 function getIgnoredNetworks() {
-    console.log("Trying get the ignored networks")
-    client.getIgnoredNetworks(new Empty(), {}, function(err, response) {
+    console.log('Trying get the ignored networks');
+    client.getIgnoredNetworks(new Empty(), {}, function (err, response) {
         if (err) {
-            console.error("Got err: ", err)
+            console.error('Got err: ', err);
             return;
         }
 
-        let ignoredList = document.getElementById("ignored_networks_list");
+        let ignoredList = document.getElementById('ignored_networks_list');
         ignoredList.innerHTML = '';
-        let table = document.createElement("table");
+        let table = document.createElement('table');
         let ssidList = response.getNamesList();
         for (const ssid of ssidList) {
             let row = document.createElement('tr');
             let data = document.createElement('td');
             data.textContent = ssid;
-            row.appendChild(data)
-            table.appendChild(row)
+            row.appendChild(data);
+            table.appendChild(row);
         }
 
         ignoredList.appendChild(table);
-    })
+    });
 }
 
 function deauthNetwork() {
-    console.log("Trying to deauth net")
+    console.log('Trying to deauth net');
     let selectedNetwork = getSelectedNetwork();
-    let network = new NetworkName();
-    network.setSsid(selectedNetwork);
-    let request = new DeauthRequest();
-    request.setNetwork(network);
-    request.setUserAddr("ff:ff:ff:ff:ff:ff"); // for now broadcast, the user should select from list of clients
+    let request = new NetworkName();
+    request.setSsid(selectedNetwork);
 
-    console.log("Sending deauth request for", selectedNetwork);
+    console.log('Sending deauth request for', selectedNetwork);
     client.deauthNetwork(request, {}, (err, response) => {
         if (err) {
-            console.error("Got err: ", err)
+            console.error('Got err: ', err);
             return;
         }
 
-        console.log("got resp");
-        // This doesn't return anythin
-    })
+        console.log('got resp');
+        // This doesn't return anything
+    });
 }
 
 function focusNetwork() {
@@ -307,7 +315,6 @@ function getStreamFromRecording() {
         cancelBtn.className = "btn btn-primary";
     })
 }
-
 function saveStream() {
     console.log("Saving stream...");
     let network = getSelectedNetwork();
@@ -321,7 +328,7 @@ function saveStream() {
         }
 
         console.log("got resp")
-        // empty resp 
+        // empty resp
     })
 }
 
