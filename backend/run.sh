3855eb98c663270cc713ecfef4eb97a1862788f3
TypicalAM <tpam_piaseczny@protonmail.com>
2024-01-16 10:16:15 +0100

    Better dev docker run script

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/opt/cmake/bin/cmake -G Ninja -B build . && ninja -C build && ./build/sniffsniff file pcap/wpa_induction.pcap
+protoc -I ../protos --grpc_out=src --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ../protos/packets.proto
+protoc -I ../protos --cpp_out=src ../protos/packets.proto
+/opt/cmake/bin/cmake -G Ninja -B build .
+ninja -C build
+./build/sniffsniff --fromfile --filename=pcap/wpa_induction.pcap
