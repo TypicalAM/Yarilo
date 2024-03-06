c8f89679cb6e8564acce43442111b58c0b46568e
TypicalAM <tpam_piaseczny@protonmail.com>
2024-03-06 21:18:05 +0100

    More funky components

---
+++ frontend/src/lib/components/aplist.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	console.log('Created networklist');
+</script>
+
+<div
+	class="hover:bg-primary/9 h-64 w-64 flex-col justify-center rounded-md bg-primary text-sm font-medium text-primary-foreground ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+>
+	<div></div>
+</div>
