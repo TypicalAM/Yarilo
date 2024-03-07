0566042ce6c3b778a6ad0829b2ab3ff602ac0e7a
TypicalAM <tpam_piaseczny@protonmail.com>
2024-03-07 03:41:16 +0100

    Partial network list

--- frontend/src/lib/components/aplist.svelte
+++ frontend/src/lib/components/aplist.svelte
@@ -1,9 +1,57 @@
 <script lang="ts">
-	console.log('Created networklist');
+	export let errMsg: string = '';
+	export let focusedNetwork: string | undefined;
+	export let networkList: string[] = [];
+
+	import Reload from 'svelte-radix/Reload.svelte';
+
+	import { client } from '$stores';
+	import { Separator } from '$lib/components/ui/separator';
+	import { Switch } from '$lib/components/ui/switch';
+	import { Label } from '$lib/components/ui/label';
+	import { Button } from '$lib/components/ui/button';
+
+	import type { RpcError, FinishedUnaryCall } from '@protobuf-ts/runtime-rpc';
+	import type { Empty, NetworkList } from '$lib/proto/packets';
+
+	const displayError = (error: RpcError) => {
+		console.error('Error!', error);
+		errMsg = error.code;
+		setTimeout(() => {
+			errMsg = '';
+		}, 3000);
+	};
+
+	const refreshAccessPoints = () => {
+		$client
+			.getAllAccessPoints({})
+			.then((data: FinishedUnaryCall<Empty, NetworkList>) => {
+				console.log('New network list fetched!', networkList);
+				networkList = data.response.names;
+			})
+			.catch(displayError);
+	};
 </script>
 
-<div
-	class="hover:bg-primary/9 h-64 w-64 flex-col justify-center rounded-md bg-primary text-sm font-medium text-primary-foreground ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
->
-	<div></div>
+<div class="h-72 w-64 rounded-xl border bg-primary p-4 text-primary-foreground">
+	<div class="mb-6 mt-1 flex h-4 items-center justify-between">
+		<h4 class="text-md px-2 font-medium leading-none">Available networks</h4>
+		<Button on:click={refreshAccessPoints} class="mt-1 hover:bg-secondary/30" size="icon">
+			<Reload class="w-4 animate-spin" />
+		</Button>
+	</div>
+
+	{#each networkList as ap}
+		<div class="mx-2 flex items-center justify-between">
+			<Label for={ap}>{ap}</Label>
+			<Switch
+				id={ap}
+				onCheckedChange={(newState) => {
+					console.log('New focus change:', newState, ap);
+					focusedNetwork = newState ? ap : '';
+				}}
+			/>
+		</div>
+		<Separator class="my-2" />
+	{/each}
 </div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,25 +1,58 @@
 <script lang="ts">
-	import { client } from '$stores';
+	import Error from '$components/error.svelte';
+	import APList from '$components/aplist.svelte';
 
-	import { Button } from '$lib/components/ui/button';
-	import { ExclamationTriangle } from 'radix-icons-svelte';
-	import * as Alert from '$lib/components/ui/alert';
+	import { client } from '$lib/stores';
+	import type { RpcError } from '@protobuf-ts/runtime-rpc';
 
-	const fetchAvailableNetworks = async () => {
-		console.log('Hello from the button');
-		let { response } = await $client.getAllAccessPoints({});
-		console.log(response);
+	let errMsg: string = ''; // they are reactive! changing them triggers a rerender
+	let networkList: string[] = [];
+	let focusCandidate: string | undefined;
+	let focusedNetwork: string = '';
+
+	const displayError = (error: RpcError) => {
+		console.error('Error!', error);
+		errMsg = error.code;
+		setTimeout(() => {
+			errMsg = '';
+		}, 3000);
 	};
+
+	const focusNetwork = (ap: string) => {
+		$client
+			.focusNetwork({ ssid: ap })
+			.then(() => {
+				console.log('Network focused!', ap);
+				focusedNetwork = ap;
+			})
+			.catch(displayError);
+	};
+
+	const unfocusNetwork = () => {
+		$client
+			.stopFocus({})
+			.then(() => {
+				console.log('Network unfocused!');
+				focusedNetwork = '';
+			})
+			.catch(displayError);
+	};
+
+	$: {
+		if (focusCandidate !== undefined) {
+			if (focusCandidate.length !== 0) {
+				focusNetwork(focusCandidate);
+			} else {
+				unfocusNetwork();
+			}
+		}
+	}
 </script>
 
-<h1>This will be the homepage i guess</h1>
+<APList bind:errMsg bind:focusedNetwork={focusCandidate} {networkList} />
 
-<Button on:click={fetchAvailableNetworks}>Get available networks</Button>
+<p>Focused network (after gprc call): "{focusedNetwork}"</p>
 
-<Alert.Root variant="destructive" class="absolute bottom-0 right-0 m-4 w-1/3">
-	<ExclamationTriangle class="h-4 w-4" />
-	<Alert.Title>This is an error!</Alert.Title>
-	<Alert.Description
-		>There has been an error with the API, what do we do now? Why isn't this popup dark?</Alert.Description
-	>
-</Alert.Root>
+{#if errMsg.length !== 0}
+	<Error message={errMsg} />
+{/if}
