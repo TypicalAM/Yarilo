c61d74782a27cd8ba0e0e5d59f16fba9090a3ad5
TypicalAM <tpam_piaseczny@protonmail.com>
2024-10-18 20:19:34 +0200

    Development build path fix and version fixes

--- backend/flake.nix
+++ backend/flake.nix
@@ -19,22 +19,23 @@
         # Runtime package
         packages.Yarilo = (with pkgs; stdenv.mkDerivation {
           pname = "Yarilo";
-          version = "0.6";
+          version = "0.8";
           allSrc = pkgs.fetchFromGitHub {
-              owner = "TypicalAM";
-              repo = "Yarilo";
-              rev = "v0.6";
-              hash = "sha256-bgvMa5bEm01lPqsHbOWkW9P4cjAmnekWYDtTP5PmlfQ=";
+            owner = "TypicalAM";
+            repo = "Yarilo";
+            rev = "v0.8";
+            hash = "sha256-sZOB6Lpbsl+nyJEaWjtUxMId+c1g67ekBk+kxGT2ITQ=";
           };
-          src = pkgs.fetchFromGitHub {
+          src = pkgs.fetchFromGitHub
+            {
               owner = "TypicalAM";
               repo = "Yarilo";
-              rev = "v0.6";
-              hash = "sha256-bgvMa5bEm01lPqsHbOWkW9P4cjAmnekWYDtTP5PmlfQ=";
-          } + "/backend";
+              rev = "v0.8";
+              hash = "sha256-sZOB6Lpbsl+nyJEaWjtUxMId+c1g67ekBk+kxGT2ITQ=";
+            } + "/backend";
 
           nativeBuildInputs = [ doxygen clang-tools gdb cmake ninja pkg-config spdlog grpc libtins protobuf openssl libpcap aircrack-ng iw libnl ];
-          vendorHash = "sha256-bgvMa5bEm01lPqsHbOWkW9P4cjAmnekWYDtTP5PmlfQ=";
+          vendorHash = "sha256-sZOB6Lpbsl+nyJEaWjtUxMId+c1g67ekBk+kxGT2ITQ=";
           patchPhase = ''
             cp $allSrc/protos/service.proto .
             sed -i 's|get_filename_component(hw_proto "..\/protos\/service.proto" ABSOLUTE)|get_filename_component(hw_proto "service.proto" ABSOLUTE)|g' CMakeLists.txt
