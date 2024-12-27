b6f3ef9f83bed114bd03a26cbe0950ea848333eb
KobotoJW <kubawolniak@wp.pl>
2024-12-27 19:50:27 +0000

    gui connecting to server

---
+++ gui/src/client.py
@@ -0,0 +1,29 @@
+import grpc
+import os
+import sys
+sys.path.append(os.path.dirname(__file__))
+import service_pb2, service_pb2_grpc
+
+class Client:
+    def __init__(self, host='localhost', port=9090):
+        self.channel = grpc.insecure_channel(f'{host}:{port}')
+        self.stub = service_pb2_grpc.SnifferStub(self.channel)
+
+    def is_connected(self):
+        try:
+            response = self.stub.SnifferList(service_pb2.Empty())
+            return True
+        except grpc.RpcError as e:
+            if e.code() == grpc.StatusCode.UNAVAILABLE:
+                print("gRPC Error: Unable to connect to the server.")
+            else:
+                print(f"gRPC Error: {e.details()} (Code: {e.code()})")
+            return False
+
+    def get_sniffer_list(self):
+        response = self.stub.SnifferList(service_pb2.Empty())
+        return response.sniffers
+
+    def get_access_point_list(self):
+        response = self.stub.AccessPointList(service_pb2.Empty())
+        return response.access_points
--- gui/src/main.py
+++ gui/src/main.py
@@ -1,13 +1,15 @@
 import os
-dirname = os.path.dirname(__file__)
-
 import RPi.GPIO as GPIO
 import sys
+from time import sleep
+dirname = os.path.dirname(__file__)
 sys.path.append(os.path.join(dirname, '..'))
+from PIL import Image, ImageDraw, ImageFont
 import spidev as SPI
 from lib import LCD_2inch4
-from PIL import Image, ImageDraw, ImageFont
+import client
 
+# Constants
 RST = 27
 DC = 25
 BL = 18
@@ -23,43 +25,113 @@ BUTTON_PINS = {
     5: "UP"
 }
 
-def initialize_display():
-    disp = LCD_2inch4.LCD_2inch4(spi=SPI.SpiDev(bus, device), spi_freq=10000000, rst=RST, dc=DC, bl=BL) 
-    disp.Init()
+class Display:
+    def __init__(self):
+        self.disp = LCD_2inch4.LCD_2inch4(spi=SPI.SpiDev(bus, device), spi_freq=10000000, rst=RST, dc=DC, bl=BL)
+        self.disp.Init()
+        self.disp.command(0x36)
+        self.disp.data(0x20)
+        self.disp.clear()
 
-    disp.command(0x36)
-    disp.data(0x20)
-    disp.clear()
-    return disp
+    def show_message(self, message, color="BLACK"):
+        size = (320, 240)
+        background = Image.new("RGB", size, (240, 255, 180))
+        draw = ImageDraw.Draw(background)
+        font = ImageFont.truetype(os.path.join(os.path.dirname(__file__), '../Font/Font02.ttf'), 25)
 
-def display_message(disp, message):
-    size = (320, 240)
-    background = Image.new("RGB", size, (240, 255, 180))
-    draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype(os.path.join(dirname, '../Font/Font01.ttf'), 25)
+        bbox = draw.textbbox((0, 0), message, font=font)
+        text_width = bbox[2] - bbox[0]
+        text_height = bbox[3] - bbox[1]
 
-    bbox = draw.textbbox((0, 0), message, font=font)
-    text_width = bbox[2] - bbox[0]
-    text_height = bbox[3] - bbox[1]
+        x = (size[0] - text_width) // 2
+        y = (size[1] - text_height) // 2
 
-    x = (size[0] - text_width) // 2
-    y = (size[1] - text_height) // 2
+        draw.text((x, y), message, fill=color, font=font)
+        self.disp.ShowImage(background.rotate(180))
 
-    draw.text((x, y), message, fill="BLACK", font=font)
-    disp.ShowImage(background.rotate(180))
+    def show_list(self, items, selected_index):
+        size = (320, 240)
+        background = Image.new("RGB", size, (240, 255, 180))
+        draw = ImageDraw.Draw(background)
+        font = ImageFont.truetype(os.path.join(os.path.dirname(__file__), '../Font/Font01.ttf'), 20)
 
-try:
-    GPIO.setmode(GPIO.BCM)
-    for pin in BUTTON_PINS.keys():
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        for i, item in enumerate(items):
+            color = "RED" if i == selected_index else "BLACK"
+            draw.text((10, 20 + i * 30), item, fill=color, font=font)
 
-    disp = initialize_display()
-    display_message(disp, "Press a button")
+        self.disp.ShowImage(background.rotate(180))
+    
+    def clear(self):
+        self.disp.reset()
+        self.disp.clear()
 
-    while True:
-        for pin, action in BUTTON_PINS.items():
-            if GPIO.input(pin) == GPIO.HIGH:
-                display_message(disp, f"{action} button pressed!")
-except KeyboardInterrupt:
-        disp.module_exit()
-        exit()
+
+class ListMenu:
+    def __init__(self, display):
+        self.display = display
+        self.items = ["Get sniffer list", "Get access point list", "Exit"]
+        self.selected_index = 0
+
+    def navigate(self, direction):
+        if direction == "UP":
+            self.selected_index = (self.selected_index - 1) % len(self.items)
+            sleep(0.1)
+        elif direction == "DOWN":
+            self.selected_index = (self.selected_index + 1) % len(self.items)
+            sleep(0.1)
+        self.display.show_list(self.items, self.selected_index)
+
+    def select(self):
+        selected_item = self.items[self.selected_index]
+        self.display.show_message(f"Selected: {selected_item}")
+        return selected_item
+
+    def refuse(self):
+        self.display.show_message("Action Refused!")
+
+
+class ButtonHandler:
+    def __init__(self, menu):
+        self.menu = menu
+        GPIO.setmode(GPIO.BCM)
+        for pin in BUTTON_PINS.keys():
+            GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    def listen(self):
+        while True:
+            for pin, action in BUTTON_PINS.items():
+                if GPIO.input(pin) == GPIO.HIGH:
+                    if action in ["UP", "DOWN"]:
+                        self.menu.navigate(action)
+                    elif action == "ACCEPT":
+                        selected = self.menu.select()
+                        if selected == "Exit":
+                            self.cleanup()
+                            return
+                    elif action == "REFUSE":
+                        self.menu.refuse()
+
+    def cleanup(self):
+        display.clear()
+        sys.exit()
+
+if __name__ == "__main__":
+    try:
+        display = Display()
+        menu = ListMenu(display)
+        button_handler = ButtonHandler(menu)
+        client = client.Client()
+        if not client.is_connected():
+            display.show_message("Failed to connect to server!", "RED")
+            sleep(3)
+            display.clear()
+            sys.exit()
+        else:
+            display.show_message("Connected to sniffer!", "GREEN")
+            sleep(2)
+
+        menu.navigate()
+        button_handler.listen()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        sys.exit()
