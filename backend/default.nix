59391942837d6aa481317edd6143bf8df4d3e766
TypicalAM <tpam_piaseczny@protonmail.com>
2025-01-06 17:00:33 +0100

    Bump version to v0.9.3

--- backend/default.nix
+++ backend/default.nix
@@ -2,7 +2,7 @@
 
 pkgs.stdenv.mkDerivation {
   pname = "Yarilo";
-  version = "0.9.2";
+  version = "0.9.3";
   allSrc = ../.;
   src = ./.;
 
--- frontend/default.nix
+++ frontend/default.nix
@@ -2,7 +2,7 @@
 
 pkgs.buildNpmPackage {
   name = "YariloFrontend";
-  version = "0.9.2";
+  version = "0.9.3";
   buildInputs = with pkgs; [ nodejs_18 ];
   src = ./.;
 
