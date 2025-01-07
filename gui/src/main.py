aa8eb614505e98c0bb0d03650e48ea79d7692132
KobotoJW <kubawolniak@wp.pl>
2025-01-07 16:44:47 +0000

    gui now scrollable with buttons

--- gui/src/main.py
+++ gui/src/main.py
@@ -16,6 +16,7 @@ DC = 25
 BL = 18
 bus = 0
 device = 0
+fontsize = 25
 
 BUTTON_PINS = {
     17: "ACCEPT",
@@ -33,45 +34,69 @@ class Display:
         self.disp.command(0x36)
         self.disp.data(0x20)
         self.disp.clear()
+        self.in_menu = False
+        self.scrollable = False
+        self.scroll_offset = 0
+        self.current_message = None
+        self.current_color = "BLACK"
+        self.current_lines = []
+        self.current_font = None
+        self.current_y = 0
+        self.current_text_height = 0
 
     def show_message(self, message, color="BLACK"):
+        self.in_menu = False
+        self.scrollable = False
+        self.current_message = message
+        self.current_color = color
         size = (320, 240)
         background = Image.new("RGB", size, (240, 255, 180))
         draw = ImageDraw.Draw(background)
-        fontsize = 20
         font = ImageFont.truetype(os.path.join(os.path.dirname(__file__), '../Font/Font02.ttf'), fontsize)
+        self.current_font = font
 
         max_width = size[0]
-        wrapped_text = textwrap.fill(message, width=max_width // fontsize*2)
+        wrapped_text = textwrap.fill(message, width=max_width // fontsize * 2)
 
         lines = wrapped_text.split('\n')
+        self.current_lines = lines
         text_height = sum([fontsize for line in lines])
+        self.current_text_height = text_height
 
         y = (size[1] - text_height) // 2
+        self.current_y = y
 
         if text_height > size[1]:
-            scroll_step = 5
-            for offset in range(0, text_height - size[1] + scroll_step, scroll_step):
-                background = Image.new("RGB", size, (240, 255, 180))
-                draw = ImageDraw.Draw(background)
-                y_offset = y - offset
-                for line in lines:
-                    draw.text((5, y_offset), line, fill=color, font=font)
-                    y_offset += fontsize
-                self.disp.ShowImage(background.rotate(180))
-                sleep(0.1)
+            self.scrollable = True
+            self.draw_text()
         else:
+            self.scrollable = False
             y_offset = y
             for line in lines:
                 draw.text((5, y_offset), line, fill=color, font=font)
                 y_offset += fontsize
             self.disp.ShowImage(background.rotate(180))
 
-    def show_list(self, items, selected_index):
+    def draw_text(self):
         size = (320, 240)
         background = Image.new("RGB", size, (240, 255, 180))
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype(os.path.join(os.path.dirname(__file__), '../Font/Font02.ttf'), 20)
+        y_offset = self.current_y - self.scroll_offset
+        for line in self.current_lines:
+            draw.text((5, y_offset), line, fill=self.current_color, font=self.current_font)
+            y_offset += fontsize
+        self.disp.ShowImage(background.rotate(180))
+
+    def refresh(self):
+        if self.current_message:
+            self.draw_text()
+
+    def show_list(self, items, selected_index):
+        self.in_menu = True
+        size = (320, 240)
+        background = Image.new("RGB", size, (240, 255, 180))
+        draw = ImageDraw.Draw(background)
+        font = ImageFont.truetype(os.path.join(os.path.dirname(__file__), '../Font/Font02.ttf'), fontsize)
 
         for i, item in enumerate(items):
             color = "RED" if i == selected_index else "BLACK"
@@ -97,6 +122,9 @@ class ListMenu:
         elif direction == "DOWN":
             self.selected_index = (self.selected_index + 1) % len(self.items)
             sleep(0.1)
+        elif direction == "NONE":
+            self.selected_index = self.selected_index
+            sleep(0.1)
         self.display.show_list(self.items, self.selected_index)
 
     def select(self):
@@ -120,8 +148,15 @@ class ButtonHandler:
         while True:
             for pin, action in BUTTON_PINS.items():
                 if GPIO.input(pin) == GPIO.HIGH:
-                    if action in ["UP", "DOWN"]:
+                    if action in ["UP", "DOWN"] and display.in_menu == True:
                         self.menu.navigate(action)
+                    elif action in ["UP", "DOWN"] and display.scrollable == True:
+                        if action == "UP":
+                            self.display.scroll_offset -= 20
+                            self.display.refresh()
+                        elif action == "DOWN":
+                            self.display.scroll_offset += 20
+                            self.display.refresh()
                     elif action == "ACCEPT":
                         selected = self.menu.select()
                         if selected == "Get sniffer list":
@@ -133,8 +168,9 @@ class ButtonHandler:
                         elif selected == "Exit":
                             self.cleanup()
                             return
-                    elif action == "REFUSE":
-                        self.menu.refuse()
+                    elif action == "REFUSE" and display.in_menu == False:
+                        self.menu.navigate("NONE")
+                        display.scroll_offset = 0
 
     def cleanup(self):
         display.clear()
