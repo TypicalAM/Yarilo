b77d21b5c91238f772f6702c55dd30f92c4e1a6e
TypicalAM <tpam_piaseczny@protonmail.com>
2025-01-08 19:27:12 +0100

    Cut longer logger names

--- backend/src/log_sink.cpp
+++ backend/src/log_sink.cpp
@@ -12,11 +12,14 @@ spdlog::level::level_enum global_log_level = spdlog::level::info;
 
 std::shared_ptr<spdlog::logger> get_logger(const std::string &name) {
   auto color_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+  std::string sink_name = name;
+  if (sink_name.size() > 25)
+    sink_name = sink_name.substr(0, 25);
 
-  std::shared_ptr<spdlog::logger> logger = spdlog::get(name);
+  std::shared_ptr<spdlog::logger> logger = spdlog::get(sink_name);
   if (!logger) {
     logger = std::make_shared<spdlog::logger>(
-        name,
+        sink_name,
         spdlog::sinks_init_list{global_proto_sink, std::move(color_sink)});
     logger->set_level(global_log_level);
   }
